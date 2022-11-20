a841eadc559af52741f4317d21a3b0a0c2124f98
woohm402 <woohm404@gmail.com>
2022-11-20 19:37:48 +0900

    탭바 구현

---
+++ app/src/main/java/com/suhyunmin/family_letter_android/views/AlbumFragment.kt
@@ -0,0 +1,8 @@
+package com.suhyunmin.family_letter_android.views
+
+import androidx.fragment.app.Fragment
+import com.suhyunmin.family_letter_android.R
+
+class AlbumFragment : Fragment(R.layout.fragment_album) {
+
+}
